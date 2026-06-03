9a57b196505b05b5b5f3e149922a2d69a33754c8
Antonio Anderson <antonio@ufpi.edu.br>
2026-06-02 23:35:16 -0300

    Firebase via environment variable

--- services/firestore_db.py
+++ services/firestore_db.py
@@ -1,9 +1,20 @@
+import os
+import json
 import firebase_admin
-from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase.json")
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 if not firebase_admin._apps:
+
+    firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+
+    if firebase_credentials:
+        cred_dict = json.loads(firebase_credentials)
+        cred = credentials.Certificate(cred_dict)
+    else:
+        cred = credentials.Certificate("firebase.json")
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
